2254e48d7407377e9cbd4b2dd00c89ccd1692e41
Canbert <thompus987@gmail.com>
2018-08-02 17:36:41 +0100

    Multiple dice

--- app/src/main/java/thompus/diceroller/DiceRollActivity.java
+++ app/src/main/java/thompus/diceroller/DiceRollActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class DiceRollActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonRoll;
     private TextView textViewResult;
+    private SeekBar seekBarAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,20 @@ public class DiceRollActivity extends AppCompatActivity implements View.OnClickL
         buttonRoll.setOnClickListener(this);
 
         textViewResult = (TextView)this.findViewById(R.id.textViewResult);
+
+        seekBarAmount = (SeekBar)this.findViewById(R.id.seekBarAmount);
     }
 
     public void onClick(View v){
         Dice dice = new Dice(6);
+        String str = "";
+        for(int i = 0; i <= seekBarAmount.getProgress(); i++){
+            str += roll(dice);
+        }
+        textViewResult.setText(str);
+    }
+
+    private String roll(Dice dice){
         String uni = null;
         switch(dice.roll()){
             case 1:
@@ -45,6 +57,6 @@ public class DiceRollActivity extends AppCompatActivity implements View.OnClickL
                 uni = "\u2685";
                 break;
         }
-        textViewResult.setText(uni);
+        return uni;
     }
 }
