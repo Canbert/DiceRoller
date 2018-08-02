dd3eb7ac1e0525aef56628325351edb8e1cfe3fc
Canbert <thompus987@gmail.com>
2018-08-02 14:59:27 +0100

    Dice roll click

--- app/src/main/java/thompus/diceroller/DiceRollActivity.java
+++ app/src/main/java/thompus/diceroller/DiceRollActivity.java
@@ -2,12 +2,28 @@ package thompus.diceroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class DiceRollActivity extends AppCompatActivity {
+public class DiceRollActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonRoll;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_roll);
+
+        buttonRoll = (Button)this.findViewById(R.id.buttonRoll);
+        buttonRoll.setOnClickListener(this);
+
+        textViewResult = (TextView)this.findViewById(R.id.textViewResult);
+    }
+
+    public void onClick(View v){
+        Dice dice = new Dice(6);
+        textViewResult.setText(String.valueOf(dice.roll()));
     }
 }
